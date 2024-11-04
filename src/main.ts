aee2f7bc6ed5b7cfd1fb3ae2d43bcb47da730d2c
Silver <166840363+theonesilverest@users.noreply.github.com>
2024-11-05 01:12:48 +0530

    Calculate Projection Function Added

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,29 @@ import typescriptLogo from './typescript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.ts'
 
-const calculateRate = (initial_rate, n) => {
-  return initial_rate - 0.0001 * n;
+const calculateRate = (n) => {
+  return 0.0127 - 0.0001 * n;
+};
+
+const calculatePopulation = (year) => {
+  let n = parseInt(year) - 2011;
+  let currentPopulation =  711000000;
+  let currentRate;
+
+  for (let i = 0; i < n; i++) {
+    currentRate = calculateRate(i);
+    currentPopulation = currentPopulation * (1 + currentRate);
+  }
+  
+  document.getElementById("result").value = currentPopulation;
+  console.log(currentPopulation);
+
+  return;
 };
 
 window.addEventListener("DOMContentLoaded", (event) => {
   document.getElementById("predict").addEventListener("click", () => {
-    console.log(document.getElementById("year").value);
+    calculatePopulation(document.getElementById("year").value);
   });
 })
 
@@ -21,6 +37,7 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
       <button id="predict" class="rounded-sm mx-[10%] text-white font-semibold px-3 py-1 text-xl bg-emerald-400 hover:bg-emerald-500 active:bg-emerald-500 active:text-gray-200 mt-3">
         Predict
       </button>
+      <h1 class="text-3xl font-semibold mx-[10%]" id="result"></h1>
     </div>
   </div>
 `
