27ec04b00d35a6640f649aaa39ad5bd22251fc76
Silver <166840363+theonesilverest@users.noreply.github.com>
2024-11-04 19:52:23 +0530

    Text Input Added; Button Added

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,9 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
     <div class="flex flex-col justify-center pt-20 px-[35%]">
       <h1 class="text-xl font-semibold mx-[10%]">Year:</h1>
       <input type="number" id="year" class="text-xl px-3 py-1 mx-[10%]" minlength="4" maxlength="4" />
-      <button id="predict" class="rounded-sm mx-[10%] text-white font-semibold px-3 py-1 text-xl bg-emerald-400 hover:bg-emerald-500 active:bg-green-500 mt-3">Predict</button>
+      <button id="predict" class="rounded-sm mx-[10%] text-white font-semibold px-3 py-1 text-xl bg-emerald-400 hover:bg-emerald-500 active:bg-emerald-500 active:text-gray-200 mt-3">
+        Predict
+      </button>
     </div>
   </div>
 `
